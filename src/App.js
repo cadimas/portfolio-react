950dea50a9eed6dd3568ce734cdfd582f72d1dbc
Mr.Cadima <andre.cadima.gomes@gmail.com>
2019-02-07 21:49:13 +0000

    missing modal and emailer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar";
 import "./App.css";
+import Landing from "./components/landing";
+import Portfolio from "./components/portfolio";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Around from "./components/around";
 
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <div>
+        <Navbar />
+        <Landing />
+        <Portfolio />
+        <About />
+        <Contact />
+        <Around />
+      </div>
+    );
   }
 }
 
